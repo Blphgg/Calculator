ac292f2d28b01a5bcce07e2667be08f474f2be36
bui phuong <builephuong895@gmail.com>
2023-11-17 06:13:36 +0700

    Update from1

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -28,12 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(281, 219);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 63);
+            button1.TabIndex = 0;
+            button1.Text = "Cộng";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(32, 73);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Số a:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(115, 66);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(236, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(90, 26);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Tính toán";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(31, 125);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 20);
+            label3.TabIndex = 4;
+            label3.Text = "Số b:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(32, 170);
+            label4.Name = "label4";
+            label4.Size = new Size(63, 20);
+            label4.TabIndex = 5;
+            label4.Text = "Kết quả:";
+            label4.Click += label4_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(115, 118);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(236, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(115, 163);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(236, 27);
+            textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(533, 324);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            ForeColor = Color.Blue;
+            Name = "Form1";
+            Text = "Tính toán";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox2;
+        private TextBox textBox3;
     }
 }
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -6,5 +6,10 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
